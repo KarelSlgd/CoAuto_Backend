efb23b41fd94ff35e77fdae5a3cf18179f185e33
KarelSlgd <20213tn076@utez.edu.mx>
2024-06-02 17:09:39 -0600

    change

--- insert_data_user/app.py
+++ insert_data_user/app.py
@@ -1,17 +1,30 @@
 import pymysql
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-rds_host = os.getenv('SONAR_TOKEN', 'default_value')
-if not rds_host:
-    raise ValueError("RDS_HOST environment variable is not set.")
-
+rds_host = os.environ['RDS_HOST']
+rds_user = os.environ['DB_USERNAME']
+rds_password = os.environ['DB_PASSWORD']
+rds_db = os.environ['DB_NAME']
 
 def lambda_handler(event, context):
-   return {
-        'statusCode': rds_host,
+    email = event['pathParameters'].get('email')
+    name = event['pathParameters'].get('name')
+    phone_number = event['pathParameters'].get('phone_number')
+    profile_image_url = event['pathParameters'].get('profile_image_url')
+    role = event['pathParameters'].get('role')
+    password = event['pathParameters'].get('password')
+
+    if not email or not name or not phone_number or not role or not password:
+        return {
+            'statusCode': 400,
+            'body': 'Missing parameters.'
+        }
+
+    insert_into_user(email, name, phone_number, profile_image_url, role, password)
+
+    return {
+        'statusCode': 200,
+        'body': 'Record inserted successfully.'
     }
 
 
