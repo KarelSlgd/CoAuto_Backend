d5dc89b7df864bf423d4881e39da77c4a6828320
ChristianAlonso16 <20213tn050@utez.edu.mx>
2024-06-06 16:41:48 -0600

    [Add] Insert Car

---
+++ insert_data_car/app.py
@@ -0,0 +1,141 @@
+import pymysql
+import os
+import json
+
+rds_host = os.environ['RDS_HOST']
+rds_user = os.environ['DB_USERNAME']
+rds_password = os.environ['DB_PASSWORD']
+rds_db = os.environ['DB_NAME']
+
+def lambda_handler(event, context):
+    try:
+        body = json.loads(event['body'])
+    except (TypeError, KeyError, json.JSONDecodeError):
+        return {
+            'statusCode': 400,
+            'body': 'Invalid request body.'
+        }
+
+    model = body.get('model')
+    brand = body.get('brand')
+    year = body.get('year')
+    price = body.get('price')
+    category = body.get('category')
+    fuel = body.get('fuel')
+    doors = body.get('doors')
+    motor = body.get('motor')
+    height = body.get('height')
+    width = body.get('width')
+    length = body.get('length')
+    weight = body.get('weight')
+    details = body.get('details')
+    id_status = body.get('id_status')
+
+    # Validate mandatory parameters
+    if not model or not brand or not year or not price or not category or not fuel or not doors or not motor or not height or not width or not length or not weight or not id_status:
+        return {
+            'statusCode': 400,
+            'body': 'Missing parameters.'
+        }
+
+    # Perform necessary validations
+    if len(model) > 50:
+        return {
+            'statusCode': 400,
+            'body': 'Model exceeds 50 characters.'
+        }
+
+    if len(brand) > 50:
+        return {
+            'statusCode': 400,
+            'body': 'Brand exceeds 50 characters.'
+        }
+
+    try:
+        year = int(year)
+    except ValueError:
+        return {
+            'statusCode': 400,
+            'body': 'Year must be an integer.'
+        }
+
+    try:
+        price = float(price)
+    except ValueError:
+        return {
+            'statusCode': 400,
+            'body': 'Price must be a float.'
+        }
+
+    try:
+        doors = int(doors)
+    except ValueError:
+        return {
+            'statusCode': 400,
+            'body': 'Doors must be an integer.'
+        }
+
+    try:
+        height = float(height)
+    except ValueError:
+        return {
+            'statusCode': 400,
+            'body': 'Height must be a float.'
+        }
+
+    try:
+        width = float(width)
+    except ValueError:
+        return {
+            'statusCode': 400,
+            'body': 'Width must be a float.'
+        }
+
+    try:
+        length = float(length)
+    except ValueError:
+        return {
+            'statusCode': 400,
+            'body': 'Length must be a float.'
+        }
+
+    try:
+        weight = float(weight)
+    except ValueError:
+        return {
+            'statusCode': 400,
+            'body': 'Weight must be a float.'
+        }
+
+    response = insert_into_car(model, brand, year, price, category, fuel, doors, motor, height, width, length, weight, details, id_status)
+
+    return response
+
+
+def insert_into_car(model, brand, year, price, category, fuel, doors, motor, height, width, length, weight, details, id_status):
+    connection = pymysql.connect(
+        host=rds_host,
+        user=rds_user,
+        password=rds_password,
+        database=rds_db
+    )
+
+    try:
+        with connection.cursor() as cursor:
+            insert_query = """INSERT INTO car (model, brand, year, price, category, fuel, doors, motor, height, width, length, weight, details, id_status)  VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,3)"""
+            cursor.execute(insert_query, (model, brand, year, price, category, fuel, doors, motor, height, width, length, weight, details, id_status))
+            connection.commit()
+
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': f'An error occurred: {str(e)}'
+        }
+
+    finally:
+        connection.close()
+
+    return {
+        'statusCode': 200,
+        'body': 'Record inserted successfully.'
+    }
