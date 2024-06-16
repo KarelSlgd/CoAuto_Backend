ffebeed79bae762a247df99ac77ebb2449e84d73
KarelSlgd <20213tn076@utez.edu.mx>
2024-06-16 17:23:57 -0600

    [FIX] register

--- register/app.py
+++ register/app.py
@@ -5,33 +5,6 @@ from botocore.exceptions import ClientError
 
 load_dotenv()
 
-
-def get_secret():
-
-    secret_name = "COAUTO"
-    region_name = "us-east-1"
-
-    # Create a Secrets Manager client
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
-
-    try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
-        secret = get_secret_value_response['SecretString']
-    except ClientError as e:
-        raise e
-
-    return json.loads(secret)
-
-
-secrets = get_secret()
-
-
 def lambda_handler(event, context):
     body = json.loads(event['body'])
 
