cb098c948cbdecd3d2852d2609c273a54a8d4ae2
KarelSlgd <20213tn076@utez.edu.mx>
2024-06-16 17:35:48 -0600

    [FIX] register

--- register/app.py
+++ register/app.py
@@ -5,15 +5,71 @@ from botocore.exceptions import ClientError
 
 load_dotenv()
 
-def lambda_handler(event, context):
-    body = json.loads(event['body'])
 
-    password = body['password']
-    email = body['email']
+def get_secret():
+
+    secret_name = "COAUTO"
+    region_name = "us-east-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+        secret = get_secret_value_response['SecretString']
+    except ClientError as e:
+        raise e
+
+    return json.loads(secret)
+
+
+
+
+def lambda_handler(event, context):
+    try:
+        body = json.loads(event['body'])
+
+        password = body['password']
+        email = body['email']
+
+        if not email or not password:
+            return {
+                'statusCode': 400,
+                'body': json.dumps('Missing parameters.')
+            }
+
+        secrets = get_secret()
+
+        register_user(email, password, secrets)
+
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Internal server error.')
+        }
+
+
+def register_user(email, password, secret):
+    client = boto3.client('cognito-idp')
+    response = client.sign_up(
+        ClientId=secret['COGNITO_CLIENT_ID'],
+        Username=email,
+        Password=password,
+        UserAttributes=[
+            {
+                'Name': 'email',
+                'Value': email
+            }
+        ]
+    )
 
     return {
         'statusCode': 200,
-        'body': json.dumps({
-            'message': 'User registered successfully.'
-        })
+        'body': json.dumps('User registered successfully.')
     }
