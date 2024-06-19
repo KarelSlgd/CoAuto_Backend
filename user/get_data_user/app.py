9016acfa24b9cd04a3d16b5675866294e7adfaf1
KarelSLgd <20213tn076@utez.edu.mx>
2024-06-19 10:41:06 -0600

    [FIX] conn

--- user/get_data_user/app.py
+++ user/get_data_user/app.py
@@ -1,6 +1,7 @@
 import json
 import os
 from common.connection import get_connection
+
 rds_host = os.getenv('RDS_HOST')
 rds_user = os.getenv('DB_USERNAME')
 rds_password = os.getenv('DB_PASSWORD')
