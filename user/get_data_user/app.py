bd55c91264630013d9ed19fe8a27312745e9ed02
KarelSLgd <20213tn076@utez.edu.mx>
2024-06-21 10:11:11 -0600

    [FIX] getuser

--- user/get_data_user/app.py
+++ user/get_data_user/app.py
@@ -5,46 +5,6 @@ import os
 from botocore.exceptions import ClientError
 
 
-def lambda_handler(event, context):
-    connection = get_connection()
-    users = []
-    try:
-        query = "SELECT id_user, id_cognito, email, u.name AS nameUser, lastname, r.name AS nameRole, s.value FROM user u INNER JOIN role r ON u.id_role = r.id_role INNER JOIN status s ON u.id_status = s.id_status;"
-        with connection.cursor() as cursor:
-            cursor.execute(query)
-            result = cursor.fetchall()
-            for row in result:
-                user = {
-                    'idUser': row['id_user'],
-                    'idCognito': row['id_cognito'],
-                    'email': row['email'],
-                    'name': row['nameUser'],
-                    'lastname': row['lastname'],
-                    'role': row['nameRole'],
-                    'status': row['value']
-                }
-                users.append(user)
-
-    except Exception as e:
-        return {
-            'statusCode': 500,
-            'body': json.dumps({
-                'message': f'An error occurred: {str(e)}'
-            })
-        }
-
-    finally:
-        connection.close()
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "get users",
-            "data": users
-        }),
-    }
-
-
 def get_connection():
     secrets = get_secret()
     try:
@@ -85,3 +45,43 @@ def get_secret():
         }
 
     return json.loads(secret)
+
+
+def lambda_handler(event, context):
+    connection = get_connection()
+    users = []
+    try:
+        query = "SELECT id_user, id_cognito, email, u.name AS nameUser, lastname, r.name AS nameRole, s.value FROM user u INNER JOIN role r ON u.id_role = r.id_role INNER JOIN status s ON u.id_status = s.id_status;"
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            result = cursor.fetchall()
+            for row in result:
+                user = {
+                    'idUser': row['id_user'],
+                    'idCognito': row['id_cognito'],
+                    'email': row['email'],
+                    'name': row['nameUser'],
+                    'lastname': row['lastname'],
+                    'role': row['nameRole'],
+                    'status': row['value']
+                }
+                users.append(user)
+
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({
+                'message': f'An error occurred: {str(e)}'
+            })
+        }
+
+    finally:
+        connection.close()
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "get users",
+            "data": users
+        }),
+    }
