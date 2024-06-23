ffb345a49fe73409ee429e042f9d7c7d2116ffce
KarelSlgd <20213tn076@utez.edu.mx>
2024-06-22 23:13:45 -0600

    [ADD] hash in login

--- cognito/login/app.py
+++ cognito/login/app.py
@@ -1,6 +1,9 @@
 from botocore.exceptions import ClientError
 import json
 import boto3
+import hmac
+import hashlib
+import base64
 
 
 def get_secret():
@@ -25,6 +28,12 @@ def get_secret():
         }
 
     return json.loads(secret)
+
+
+def calculate_secret_hash(client_id, secret_key, username):
+    message = username + client_id
+    dig = hmac.new(secret_key.encode('utf-8'), message.encode('utf-8'), hashlib.sha256).digest()
+    return base64.b64encode(dig).decode()
 
 
 def lambda_handler(event, context):
@@ -53,8 +62,10 @@ def lambda_handler(event, context):
 def login_auth(email, password, secret):
     try:
         client = boto3.client('cognito-idp')
+        secret_hash = calculate_secret_hash(secret['COGNITO_CLIENT_ID'], secret['SECRET_KEY'], email)
         response = client.initiate_auth(
             ClientId=secret['COGNITO_CLIENT_ID'],
+            SecretHash=secret_hash,
             AuthFlow='USER_PASSWORD_AUTH',
             AuthParameters={
                 'USERNAME': email,
