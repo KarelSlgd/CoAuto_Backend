07399f2507a42c1229726c5a5d422e13de5da41d
Victor Noe <20213tn059@utez.edu.mx>
2024-06-23 17:35:33 -0600

    [CHANGE] get one rate

--- rate/get_one_data_rate/app.py
+++ rate/get_one_data_rate/app.py
@@ -1,20 +1,20 @@
 from dotenv import load_dotenv
 import json
-import pymysql
 import os
+from .database import connect_to_db, close_connection, execute_query
 
 load_dotenv()
-
-rds_host = os.getenv('RDS_HOST')
-rds_user = os.getenv('DB_USERNAME')
-rds_password = os.getenv('DB_PASSWORD')
-rds_db = os.getenv('DB_NAME')
 
 
 def lambda_handler(event, context):
     print("Received event:", json.dumps(event))
 
-    id_auto = event['queryStringParameters'].get('id_auto')
+    if 'queryStringParameters' in event:
+        id_auto = event['queryStringParameters'].get('id_auto')
+    else:
+        body = json.loads(event.get('body', '{}'))
+        id_auto = body.get('id_auto')
+    #id_auto = event['queryStringParameters'].get('id_auto')
     #body = json.loads(event['body'])
     #id_auto = body['id_auto']
 
@@ -26,31 +26,29 @@ def lambda_handler(event, context):
             })
         }
 
-    connection = pymysql.connect(
-        host=rds_host,
-        user=rds_user,
-        password=rds_password,
-        database=rds_db
-    )
+    rds_host = os.getenv('RDS_HOST')
+    rds_user = os.getenv('DB_USERNAME')
+    rds_password = os.getenv('DB_PASSWORD')
+    rds_db = os.getenv('DB_NAME')
 
+    connection = connect_to_db(rds_host, rds_user, rds_password, rds_db)
+    query = f"SELECT id_rate, value, comment, a.model, a.brand, u.name, u.lastname FROM rate r INNER JOIN auto a ON r.id_auto=a.id_auto INNER JOIN user u ON r.id_user=u.id_user WHERE a.id_auto = {id_auto}"
     rates = []
 
     try:
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT id_rate, value, comment, a.model, a.brand, u.name, u.lastname FROM rate r INNER JOIN auto a ON r.id_auto=a.id_auto INNER JOIN user u ON r.id_user=u.id_user WHERE a.id_auto = %s", (id_auto,))
-            result = cursor.fetchall()
+        result = execute_query(connection, query)
 
-            for row in result:
-                rate = {
-                    'id_rate': row[0],
-                    'value': row[1],
-                    'comment': row[2],
-                    'model': row[3],
-                    'brand': row[4],
-                    'name': row[5],
-                    'lastname': row[6]
-                }
-                rates.append(rate)
+        for row in result:
+            rate = {
+                'id_rate': row[0],
+                'value': row[1],
+                'comment': row[2],
+                'model': row[3],
+                'brand': row[4],
+                'name': row[5],
+                'lastname': row[6]
+            }
+            rates.append(rate)
 
     except Exception as e:
         return {
@@ -61,7 +59,7 @@ def lambda_handler(event, context):
         }
 
     finally:
-        connection.close()
+        close_connection(connection)
 
     return {
         'statusCode': 200,
