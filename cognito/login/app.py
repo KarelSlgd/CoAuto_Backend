b182924945e822d4acbf497dff32d394c1c0a3dd
KarelSlgd <20213tn076@utez.edu.mx>
2024-06-26 20:43:09 -0600

    [FIX] move database to new file

--- cognito/login/app.py
+++ cognito/login/app.py
@@ -1,39 +1,6 @@
-from botocore.exceptions import ClientError
 import json
 import boto3
-import hmac
-import hashlib
-import base64
-
-
-def get_secret():
-    secret_name = "COAUTO"
-    region_name = "us-east-1"
-
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
-
-    try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
-        secret = get_secret_value_response['SecretString']
-    except ClientError as e:
-        return {
-            'statusCode': 500,
-            'body': json.dumps(f'An error occurred: {str(e)}')
-        }
-
-    return json.loads(secret)
-
-
-def calculate_secret_hash(client_id, secret_key, username):
-    message = username + client_id
-    dig = hmac.new(secret_key.encode('utf-8'), message.encode('utf-8'), hashlib.sha256).digest()
-    return base64.b64encode(dig).decode()
+from cognito.login.database import get_secret, calculate_secret_hash
 
 
 def lambda_handler(event, context):
---
+++ cognito/login/database.py
@@ -0,0 +1,35 @@
+from botocore.exceptions import ClientError
+import hmac
+import hashlib
+import base64
+import json
+import boto3
+
+def get_secret():
+    secret_name = "COAUTO"
+    region_name = "us-east-1"
+
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+        secret = get_secret_value_response['SecretString']
+    except ClientError as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps(f'An error occurred: {str(e)}')
+        }
+
+    return json.loads(secret)
+
+
+def calculate_secret_hash(client_id, secret_key, username):
+    message = username + client_id
+    dig = hmac.new(secret_key.encode('utf-8'), message.encode('utf-8'), hashlib.sha256).digest()
+    return base64.b64encode(dig).decode()
